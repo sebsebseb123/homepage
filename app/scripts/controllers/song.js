59a5002291bf4df8369fd8407ddd391af0b852b2
sebsebseb123 <sebastian.lesch@gmail.com>
2013-12-09 20:15:57 -0500

    Added Ctrls song and songs.

---
+++ app/scripts/controllers/song.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sebsebseb123comApp')
+  .controller('SongctrlCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/songs.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sebsebseb123comApp')
+  .controller('SongsctrlCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
